c87d84dc5f757fe2d9ad429797c38dc4181442d9
arcolife <arcshams@gmail.com>
2014-07-03 01:41:41 +0530

    add assignments

---
+++ TODO.md
@@ -0,0 +1,16 @@
+[ ] assignment 1
+  - [x] add sample output.txt
+  - [x] upgrade twitter stream / search API script
+  - [x] add problem_1_submission.txt
+  - [ ] execute twitter_sentiment
+  - [ ] execute term_sentiment
+
+[ ] assignment 2
+[ ] assignment 3
+[ ] cloud quiz
+
+[ ] add examples to dumps
+  - [x] anscombe's quartet.R  
+  - [x] intro.R  
+  - [x] Rplots.pdf  
+  - [x] url_readlines_kmeans.R
---
+++ assignment1/twitter_fetch.py
@@ -0,0 +1,103 @@
+import oauth2 as oauth
+import urllib2 as urllib
+import os
+import sys
+# See assignment1.html instructions or README for how to get these credentials
+
+api_key = os.environ.get("TWITTER_KEY")
+api_secret = os.environ.get("TWITTER_SECRET")
+access_token_key = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_SECRET")
+
+_debug = 0
+
+oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "GET"
+
+
+http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+'''
+Construct, sign, and open a twitter request
+using the hard-coded credentials above.
+'''
+def twitterreq(url, method, parameters):
+  req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                             token=oauth_token,
+                                             http_method=http_method,
+                                             http_url=url, 
+                                             parameters=parameters)
+
+  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+  headers = req.to_header()
+
+  if http_method == "POST":
+    encoded_post_data = req.to_postdata()
+  else:
+    encoded_post_data = None
+    url = req.to_url()
+
+  opener = urllib.OpenerDirector()
+  opener.add_handler(http_handler)
+  opener.add_handler(https_handler)
+
+  response = opener.open(url, encoded_post_data)
+
+  return response               
+
+def fetchstream():
+  url = "https://stream.twitter.com/1/statuses/sample.json"
+  parameters = []
+  response = twitterreq(url, "GET", parameters)
+  if response:
+    f = open('output.txt','wb')
+    try:
+      for line in response:
+        print line.strip()
+        f.write(line.strip())
+    except:
+      f.close()
+  else:
+    quit("\nGot no response!")
+
+
+def fetchsearched():
+  url = "https://api.twitter.com/1.1/search/tweets.json"
+  #terms = ' '.join(sys.argv[1:])
+  terms = raw_input("Enter search terms: ")
+  parameters = {
+    'q': terms,
+    'count': int(raw_input("Enter result count: "))
+  } 
+  response = twitterreq(url, "GET", parameters)
+  # for line in response:
+  #   print line.strip()
+  if response:
+    f = open('output.txt','wb')
+    try:
+      for line in response:
+        print line.strip()
+        f.write(line.strip())
+    except:
+      f.close()
+  else:
+    quit("\nGot no response!")
+      
+if __name__ == '__main__':
+  try:
+    print "1: Twitter Stream API\n2: Twitter Search API"
+    choice = int(raw_input("Enter choice(1/2): "))
+  except:
+    quit("\nWrong input!")
+  if choice == 1:
+    fetchstream()
+  elif choice == 2:
+    fetchsearched()
+  else:
+    quit("\nWrong choice!")
