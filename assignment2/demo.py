b7efe0d8b6cebb2c17466e433b0d62a59556b213
arcolife <arcshams@gmail.com>
2014-07-12 08:48:20 +0530

    complete upto 2.Q4

--- assignment2/demo.py
+++ assignment2/demo.py
@@ -6,36 +6,61 @@ con = None
 try:
     # TABLE -> frequency(docid, term, count)
     con = lite.connect('reuters.db')
+    # declare cursor
+    cur = con.cursor()    
+
+    # Q1
+    sql =  """
+    SELECT count(*) FROM (
+    SELECT * FROM frequency WHERE docid = '10398_txt_earn'
+    );  -- x is optional"""
+
+    # Q2
+    sql = """
+    SELECT count(*) FROM (
+    SELECT term FROM frequency 
+    WHERE docid = '10398_txt_earn' 
+    AND count = 1
+    );  -- x is optional"""
+
+    # Q3
+    sql = """
+    SELECT count(*) FROM (
+    SELECT term FROM frequency 
+    WHERE docid = '10398_txt_earn' 
+    AND count =1 
+    UNION
+    SELECT term FROM frequency 
+    WHERE docid = '925_txt_trade' 
+    AND count = 1
+    );  -- x is optional"""
+    
+    # Q4
+    sql = """
+    SELECT * FROM frequency
+    WHERE term LIKE 'parliament'
+    """
+
+    res = cur.execute(sql).fetchall()
+    print "{0}\n\nCount = {1}".format(res, len(res))
+    
     # frequency = {'docid' : [],
     #              'term' : [],
     #              'count' : []}
-
-    # declare cursor
-    cur = con.cursor()    
     # test command
-    cur.execute('SELECT SQLITE_VERSION()')
-    data = cur.fetchone()
-    print "SQLite version: %s" % data                
+    # cur.execute('SELECT SQLITE_VERSION()')
+    # data = cur.fetchone()
+    # print "SQLite version: %s" % data                
     
-    # entry count
-    cur.execute('SELECT count(*) FROM frequency')
-    print "Entry count: %s" % cur.fetchone()
+    # # entry count
+    # cur.execute('SELECT count(*) FROM frequency')
+    # print "Entry count: %s" % cur.fetchone()
 
-    # sample data
-    cur.execute("SELECT * FROM frequency WHERE docid = '991_txt_trade'")
-    freq = dict(zip(['docid', 'term', 'count'], cur.fetchone())) #fetchall
-    print "Sample data: \n", freq
+    # # sample data
+    # cur.execute("SELECT * FROM frequency WHERE docid = '991_txt_trade'")
+    # freq = dict(zip(['docid', 'term', 'count'], cur.fetchone())) #fetchall
+    # print "Sample data: \n", freq
 
-    ## sample commands
-    # # Create table as per requirement
-    # sql = """CREATE TABLE demo (
-    #          FIRST_NAME  CHAR(20) NOT NULL,
-    #          LAST_NAME  CHAR(20),
-    #          AGE INT,  
-    #          SEX CHAR(1),
-    #          INCOME FLOAT )"""
-    # cur.execute(sql)
-    
 except lite.Error, e:    
     print "Error %s:" % e.args[0]
     sys.exit(1)
@@ -43,5 +68,3 @@ except lite.Error, e:
 finally:    
     if con:
         con.close()
-
-
--- assignment2/sql_filename.sql
+++ assignment2/sql_filename.sql
@@ -4,5 +4,28 @@
 -- term is an English word, 
 -- and count is the number of the occurrences of the term within the document indicated by docid.
 
-SELECT * FROM frequency WHERE docid = '991_txt_trade';
-SELECT count(*) FROM frequency;
+-- Test query: SELECT * FROM frequency WHERE docid = '991_txt_trade';
+
+SELECT count(*) FROM (
+       -- Q1
+       -- SELECT * FROM frequency WHERE docid = '10398_txt_earn' 
+       
+       -- Q2
+       -- SELECT term FROM frequency 
+       -- WHERE docid = '10398_txt_earn' 
+       -- AND count = 1
+
+       -- Q3
+       -- SELECT term FROM frequency 
+       -- WHERE docid = '10398_txt_earn' 
+       -- AND count =1 
+       -- UNION
+       -- SELECT term FROM frequency 
+       -- WHERE docid = '925_txt_trade' 
+       -- AND count = 1
+       
+       -- Q4
+       SELECT * FROM frequency 
+       WHERE term LIKE 'parliament'
+       
+) x;  -- x is optional
